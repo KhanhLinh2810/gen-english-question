1058615d15a9e4da0305835bcfada994536b4bb5
KhanhLinh2810 <your_email_associated_with_KhanhLinh2810>
2025-10-16 14:28:58 +0700

    rename field context

--- src/models/comment.py
+++ src/models/comment.py
@@ -9,7 +9,7 @@ class Comment(BaseModel):
     id = Column(Integer, primary_key=True, index=True)
     question_id = Column(Integer, ForeignKey("questions.id"), nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    context = Column(String(255), nullable=False)
+    content = Column(String(255), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())  
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())  
 
--- src/models/question.py
+++ src/models/question.py
@@ -9,7 +9,7 @@ class Question(BaseModel):
     
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    context = Column(Text, nullable=False)
+    content = Column(Text, nullable=False)
     description = Column(Text, nullable=True) # danh cho cau hoi doan van
     tags = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
